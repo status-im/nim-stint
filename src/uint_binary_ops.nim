f861af840ad54d2fe10bd51fb87830b307de2267
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2018-02-16 11:38:48 +0100

    Use Nim input convention x, y instead of a,b

--- src/uint_binary_ops.nim
+++ src/uint_binary_ops.nim
@@ -4,42 +4,42 @@
 import  ./private/utils,
         uint_type
 
-proc `+=`*[T: MpUint](a: var T, b: T) {.noSideEffect.}=
+proc `+=`*[T: MpUint](x: var T, y: T) {.noSideEffect.}=
   ## In-place addition for multi-precision unsigned int
   #
   # Optimized assembly should contain adc instruction (add with carry)
   # Clang on MacOS does with the -d:release switch and MpUint[uint32] (uint64)
-  type Base = type a.lo
-  let tmp = a.lo
+  type Base = type x.lo
+  let tmp = x.lo
 
-  a.lo += b.lo
-  a.hi += (a.lo < tmp).Base + b.hi
+  x.lo += y.lo
+  x.hi += (x.lo < tmp).Base + y.hi
 
-proc `+`*[T: MpUint](a, b: T): T {.noSideEffect, noInit, inline.}=
+proc `+`*[T: MpUint](x, y: T): T {.noSideEffect, noInit, inline.}=
   # Addition for multi-precision unsigned int
-  result = a
-  result += b
+  result = x
+  result += y
 
-proc `-=`*[T: MpUint](a: var T, b: T) {.noSideEffect.}=
+proc `-=`*[T: MpUint](x: var T, y: T) {.noSideEffect.}=
   ## In-place substraction for multi-precision unsigned int
   #
   # Optimized assembly should contain sbb instruction (substract with borrow)
   # Clang on MacOS does with the -d:release switch and MpUint[uint32] (uint64)
-  type MPBase = type a.lo
-  let tmp = a.lo
+  type MPBase = type x.lo
+  let tmp = x.lo
 
-  a.lo -= b.lo
-  a.hi -= (a.lo > tmp).MPBase + b.hi
+  x.lo -= y.lo
+  x.hi -= (x.lo > tmp).MPBase + y.hi
 
-proc `-`*[T: MpUint](a, b: T): T {.noSideEffect, noInit, inline.}=
+proc `-`*[T: MpUint](x, y: T): T {.noSideEffect, noInit, inline.}=
   # Substraction for multi-precision unsigned int
-  result = a
-  result -= b
+  result = x
+  result -= y
 
-proc naiveMul[T: BaseUint](a, b: T): MpUint[T] {.noSideEffect, noInit, inline.}
+proc naiveMul[T: BaseUint](x, y: T): MpUint[T] {.noSideEffect, noInit, inline.}
   # Forward declaration
 
-proc `*`*[T: MpUint](a, b: T): T {.noSideEffect, noInit.}=
+proc `*`*[T: MpUint](x, y: T): T {.noSideEffect, noInit.}=
   ## Multiplication for multi-precision unsigned uint
   #
   # For our representation, it is similar to school grade multiplication
@@ -59,8 +59,8 @@ proc `*`*[T: MpUint](a, b: T): T {.noSideEffect, noInit.}=
   # For T * T --> T we don't need to compute z2 as it always overflow
   # For T * T --> 2T (uint64 * uint64 --> uint128) we use extra precision multiplication
 
-  result = naiveMul(a.lo, b.lo)
-  result.hi += (naiveMul(a.hi, b.lo) + naiveMul(a.lo, b.hi)).lo
+  result = naiveMul(x.lo, y.lo)
+  result.hi += (naiveMul(x.hi, y.lo) + naiveMul(x.lo, y.hi)).lo
 
 template naiveMulImpl[T: MpUint](x, y: T): MpUint[T] =
   # See details at
@@ -89,12 +89,12 @@ template naiveMulImpl[T: MpUint](x, y: T): MpUint[T] =
   result.lo = z1.lo shl halfShl + z0
   result.hi = z2 + z1.hi
 
-proc naiveMul[T: BaseUint](a, b: T): MpUint[T] {.noSideEffect, noInit, inline.}=
+proc naiveMul[T: BaseUint](x, y: T): MpUint[T] {.noSideEffect, noInit, inline.}=
   ## Naive multiplication algorithm with extended precision
 
   when T.sizeof in {1, 2, 4}:
     # Use types twice bigger to do the multiplication
-    cast[type result](a.asDoubleUint * b.asDoubleUint)
+    cast[type result](x.asDoubleUint * y.asDoubleUint)
 
   elif T.sizeof == 8: # uint64 or MpUint[uint32]
     # We cannot double uint64 to uint128
