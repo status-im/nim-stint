a809768854056b755b18f4dcbaf4e02258c1d653
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2018-03-26 16:05:19 +0200

    Fix https://github.com/status-im/mpint/issues/7 17% speedup on modular division

--- src/private/uint_comparison.nim
+++ src/private/uint_comparison.nim
@@ -7,18 +7,54 @@
 #
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import  ./uint_type
+import  ./uint_type, macros
 
-proc `<`*(x, y: MpUintImpl): bool {.noSideEffect, noInit, inline.}=
-  (x.hi < y.hi) or ((x.hi == y.hi) and x.lo < y.lo)
+macro cast_optim(x: typed): untyped =
+  # Size of doesn't always work at compile-time, pending PR https://github.com/nim-lang/Nim/pull/5664
 
-proc `<=`*(x, y: MpUintImpl): bool {.noSideEffect, noInit, inline.}=
-  # Lower or equal comparison for multi-precision integers
-  result = if x == y: true
-           else: x < y
+  var multiplier = 1
+  var node = x.getTypeInst
+
+  while node.kind == nnkBracketExpr:
+    assert eqIdent(node[0], "MpuintImpl")
+    multiplier *= 2
+    node = node[1]
+
+  # node[1] has the type
+  # size(node[1]) * multiplier is the size in byte
+
+  # For optimization we cast to the biggest possible uint
+  if eqIdent(node, "uint64"):
+    multiplier = multiplier div 8
+    result = quote do:
+      cast[array[multiplier, uint64]](x)
+  elif eqIdent(node, "uint32"):
+    # Why would someone do a MpuintImpl[MpUintImpl[uint32]]?
+    assert multiplier == 1
+    result = quote do:
+      cast[uint32](x)
+  elif eqIdent(node, "uint16"):
+    # Why would someone do a MpuintImpl[MpUintImpl[uint16]]?
+    assert multiplier == 1
+    result = quote do:
+      cast[uint16](x)
 
 proc isZero*(n: SomeUnsignedInt): bool {.noSideEffect,inline.} =
   n == 0
 
 proc isZero*(n: MpUintImpl): bool {.noSideEffect,inline.} =
   n.lo.isZero and n.hi.isZero
+
+proc `<`*(x, y: MpUintImpl): bool {.noSideEffect, noInit, inline.}=
+  (x.hi < y.hi) or ((x.hi == y.hi) and x.lo < y.lo)
+
+proc `==`*(x, y: MpuintImpl): bool {.noSideEffect, noInit, inline.}=
+  # Equal comparison for multi-precision integers
+
+  # We cast to array of bytes because the default comparison is slow
+  result = cast_optim(x) == cast_optim(y)
+
+proc `<=`*(x, y: MpUintImpl): bool {.noSideEffect, noInit, inline.}=
+  # Lower or equal comparison for multi-precision integers
+  result = if x == y: true
+           else: x < y
