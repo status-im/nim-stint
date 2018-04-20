fab45af9d3fb6485d619bbdb16b5da8bca012166
mratsim <mamy_github@numforge.co>
2018-04-20 12:59:15 +0200

    Managed to get best borrow code for the not inlined substraction #10

--- src/private/uint_binary_ops.nim
+++ src/private/uint_binary_ops.nim
@@ -17,32 +17,30 @@ import  ./bithacks, ./conversion,
 
 proc `+=`*(x: var MpUintImpl, y: MpUintImpl) {.noSideEffect, inline.}=
   ## In-place addition for multi-precision unsigned int
-  #
-  # Optimized assembly should contain adc instruction (add with carry)
-  # Clang on MacOS does with the -d:release switch and MpUint[uint32] (uint64)
+
   type SubTy = type x.lo
   x.lo += y.lo
   x.hi += (x.lo < y.lo).toSubtype(SubTy) + y.hi
 
 proc `+`*(x, y: MpUintImpl): MpUintImpl {.noSideEffect, noInit, inline.}=
   # Addition for multi-precision unsigned int
+
   result = x
   result += y
 
 proc `-=`*(x: var MpUintImpl, y: MpUintImpl) {.noSideEffect, inline.}=
   ## In-place substraction for multi-precision unsigned int
-  #
-  # Optimized assembly should contain sbb instruction (substract with borrow)
-  # Clang on MacOS does with the -d:release switch and MpUint[uint32] (uint64)
+
   type SubTy = type x.lo
   x.hi -= (x.lo < y.lo).toSubtype(SubTy) + y.hi
   x.lo -= y.lo
 
 proc `-`*(x, y: MpUintImpl): MpUintImpl {.noSideEffect, noInit, inline.}=
   # Substraction for multi-precision unsigned int
-  result = x
-  result -= y
 
+  type SubTy = type x.lo
+  result.lo = x.lo - y.lo
+  result.hi = x.hi - y.hi - (x.lo < y.lo).toSubtype(SubTy)
 
 # ################### Multiplication ################### #
 
