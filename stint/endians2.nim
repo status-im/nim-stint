a4d7d4e713d8eb47846ccbed676ec7a2f57456b5
Jacek Sieka <jacek@status.im>
2024-11-27 12:18:57 +0100

    Refresh endians2
    * speed up conversions by unrolling bulk limb conversion
    * remove default from `toBytes`/`fromBytes` - the existing default did
    not match `stew/endians2` which is an unnecessary risk - rather than
    changing it to match, the safer option is to remove it for now so that
    code in the wild gets a compile error
    * deprecate redundant names like `toByteArrayXX` (`toBytesXX` etc do the
    same thing)
    * deprecate staticFor (now lives in stew)

--- stint/endians2.nim
+++ stint/endians2.nim
@@ -7,275 +7,295 @@
 #
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import private/datatypes
+import stew/[assign2, endians2, staticfor], private/datatypes
 
-{.push raises: [], noinit, gcsafe.}
+{.push raises: [], noinit, gcsafe, inline.}
 
 # Serialization
 # ------------------------------------------------------------------------------------------
 
-template toByte(x: SomeUnsignedInt): byte =
-  ## At compile-time, conversion to bytes checks the range
-  ## we want to ensure this is done at the register level
-  ## at runtime in a single "mov byte" instruction
-  when nimvm:
-    byte(x and 0xFF)
-  else:
-    byte(x)
+template toBytesCopy(src: StUint) =
+  # Copy src to result maintaining native byte order
+  const words = result.len div sizeof(Word)
+  staticFor i, 0 ..< words:
+    let limb = src.limbs[i].toBytes()
+    const pos = i * sizeof(Word)
+    assign(result.toOpenArray(pos, pos + sizeof(Word) - 1), limb)
 
-template blobFrom(dst: var openArray[byte], src: SomeUnsignedInt, startIdx: int, endian: static Endianness) =
-  ## Write an integer into a raw binary blob
-  ## Swapping endianness if needed
-  when endian == cpuEndian:
-    for i in 0 ..< sizeof(src):
-      dst[startIdx+i] = toByte((src shr (i * 8)))
-  else:
-    for i in 0 ..< sizeof(src):
-      dst[startIdx+sizeof(src)-1-i] = toByte((src shr (i * 8)))
+  const leftover = result.len - (words * sizeof(Word))
+  when leftover > 0:
+    # Handle partial limb when bits don't line up with word size
+    let limb = src.limbs[^1].toBytes()
+    assign(
+      result.toOpenArray(result.len - leftover, result.len - 1),
+      limb.toOpenArray(0, leftover - 1),
+    )
+
+template toBytesSwap(src: StUint) =
+  # Copy src to result swapping both limb and byte order
+  const words = result.len div sizeof(Word)
+  staticFor i, 0 ..< words:
+    let limb = swapBytes(src.limbs[i]).toBytes()
+    const pos = result.len - (i + 1) * sizeof(Word)
+    assign(result.toOpenArray(pos, pos + sizeof(Word) - 1), limb)
+
+  const leftover = result.len - (words * sizeof(Word))
+  when leftover > 0:
+    let limb = swapBytes(src.limbs[^1]).toBytes()
+    assign(
+      result.toOpenArray(0, leftover - 1),
+      limb.toOpenArray(limb.len - leftover, limb.len - 1),
+    )
 
 func toBytesLE*[bits: static int](src: StUint[bits]): array[bits div 8, byte] =
-  var
-    src_idx, dst_idx = 0
-    acc: Word = 0
-    acc_len = 0
-
-  when cpuEndian == bigEndian:
-    srcIdx = src.limbs.len - 1
-
-  var tail = result.len
-  while tail > 0:
-    when cpuEndian == littleEndian:
-      let w = if src_idx < src.limbs.len: src.limbs[src_idx]
-              else: 0
-      inc src_idx
-    else:
-      let w = if src_idx >= 0: src.limbs[src_idx]
-              else: 0
-      dec src_idx
-
-    if acc_len == 0:
-      # We need to refill the buffer to output 64-bit
-      acc = w
-      acc_len = WordBitWidth
-    else:
-      let lo = acc
-      acc = w
-
-      if tail >= sizeof(Word):
-        # Unrolled copy
-        result.blobFrom(src = lo, dst_idx, littleEndian)
-        dst_idx += sizeof(Word)
-        tail -= sizeof(Word)
-      else:
-        # Process the tail and exit
-        when cpuEndian == littleEndian:
-          # When requesting little-endian on little-endian platform
-          # we can just copy each byte
-          # tail is inclusive
-          for i in 0 ..< tail:
-            result[dst_idx+i] = toByte(lo shr (i*8))
-        else: # TODO check this
-          # We need to copy from the end
-          for i in 0 ..< tail:
-            result[dst_idx+i] = toByte(lo shr ((tail-i)*8))
-        return
-
-func toBytesBE*[bits: static int](src: StUint[bits]): array[bits div 8, byte] {.inline.} =
-  var
-    src_idx = 0
-    acc: Word = 0
-    acc_len = 0
-
-  when cpuEndian == bigEndian:
-    srcIdx = src.limbs.len - 1
-
-  var tail = result.len
-  while tail > 0:
-    when cpuEndian == littleEndian:
-      let w = if src_idx < src.limbs.len: src.limbs[src_idx]
-              else: 0
-      inc src_idx
-    else:
-      let w = if src_idx >= 0: src.limbs[src_idx]
-              else: 0
-      dec src_idx
-
-    if acc_len == 0:
-      # We need to refill the buffer to output 64-bit
-      acc = w
-      acc_len = WordBitWidth
-    else:
-      let lo = acc
-      acc = w
-
-      if tail >= sizeof(Word):
-        # Unrolled copy
-        tail -= sizeof(Word)
-        result.blobFrom(src = lo, tail, bigEndian)
-      else:
-        # Process the tail and exit
-        when cpuEndian == littleEndian:
-          # When requesting little-endian on little-endian platform
-          # we can just copy each byte
-          # tail is inclusive
-          for i in 0 ..< tail:
-            result[tail-1-i] = toByte(lo shr (i*8))
-        else:
-          # We need to copy from the end
-          for i in 0 ..< tail:
-            result[tail-1-i] = toByte(lo shr ((tail-i)*8))
-        return
-
-func toBytes*[bits: static int](x: StUint[bits], endian: Endianness = bigEndian): array[bits div 8, byte] {.inline.} =
-  ## Default to bigEndian
-  if endian == littleEndian:
-    result = x.toBytesLE()
+  ## Encode `src` to a byte array using little-endian byte order
+  when cpuEndian == littleEndian:
+    toBytesCopy(src)
   else:
-    result = x.toBytesBE()
+    toBytesSwap(src)
+
+func toBytesBE*[bits: static int](src: StUint[bits]): array[bits div 8, byte] =
+  ## Encode `src` to a byte array using big-endian byte order
+  when cpuEndian == littleEndian:
+    toBytesSwap(src)
+  else:
+    toBytesCopy(src)
+
+func toBytes*[bits: static int](
+    x: StUint[bits], endian: Endianness
+): array[bits div 8, byte] =
+  ## Encode `src` to a byte array using the given byte order
+  ## TODO Unlike the corresponding function in stew/endians that defaults to
+  ##      native endian, this function used to default to big endian - the
+  ##      default has been removed to avoid confusion and runtime surprises
+  if endian == littleEndian:
+    x.toBytesLE()
+  else:
+    x.toBytesBE()
 
 # Deserialization
 # ------------------------------------------------------------------------------------------
 
+when sizeof(Word) == 8:
+  const
+    wordMask = 0b111
+    wordShift = 3
+elif sizeof(Word) == 4:
+  const
+    wordMask = 0b11
+    wordShift = 2
+else:
+  static:
+    raiseAssert "unsupported"
+
+template fromBytesSwapUnrolled(src: openArray[byte]) =
+  # Copy src to result swapping limb and byte order - unrolled version for when
+  # src doers not need padding
+  const
+    bytes = result.bits div 8
+    words = bytes shr wordShift
+
+  staticFor i, 0 ..< words:
+    const pos = bytes - (i + 1) * sizeof(Word)
+    let limb = Word.fromBytes(src.toOpenArray(pos, pos + sizeof(Word) - 1))
+    result.limbs[i] = swapBytes(limb)
+
+  # Handle partial limb in case the bit length doesn't line up with word size
+  const leftover = bytes mod sizeof(Word)
+  when leftover > 0:
+    var limb: Word
+    staticFor i, 0 ..< leftover:
+      limb = limb or (Word(src[i]) shl ((leftover - i - 1) shl 3))
+
+    result.limbs[words] = limb
+
+template fromBytesSwap(src: openArray[byte]) =
+  # Copy src to result swapping limb and byte order
+  if src.len >= result.bits div 8:
+    # Fast path - there's enough bytes for the full stint meaning we don't have
+    # to worry about out-of-bounds src access
+    fromBytesSwapUnrolled(src)
+  else:
+    # Slow path - src is shorter than the integer so we must zero-pad
+    let
+      bytes = src.len
+      words = bytes shr wordShift
+
+    block bulk: # Unroll this part to reduce in-loop arithmetic
+      staticFor i, 0 ..< result.limbs.len:
+        if i >= words:
+          break bulk
+        const reversePos = (i + 1) * sizeof(Word)
+        let
+          pos = bytes - reversePos
+          limb = Word.fromBytes(src.toOpenArray(pos, pos + sizeof(Word) - 1))
+        result.limbs[i] = swapBytes(limb)
+
+    # Handle partial limb in case the bit length doesn't line up with word size
+    let leftover = bytes and wordMask
+    var limb: Word
+    for i in 0 ..< leftover:
+      limb = limb or (Word(src[i]) shl ((leftover - i - 1) shl 3))
+
+    result.limbs[words] = limb
+
+    # noinit means we have to manually zero the limbs missing from src
+    for i in words + 1 ..< result.limbs.len:
+      result.limbs[i] = 0
+
+template fromBytesCopyUnrolled(src: openArray[byte]) =
+  # Copy src to result maintaining limb and byte order - unrolled version for when
+  # src doers not need padding
+  const
+    bytes = result.bits div 8
+    words = bytes shr wordShift
+
+  staticFor i, 0 ..< words:
+    const pos = i * sizeof(Word)
+    let limb = Word.fromBytes(src.toOpenArray(pos, pos + sizeof(Word) - 1))
+    result.limbs[i] = limb
+
+  const leftover = bytes and wordMask
+  when leftover > 0:
+    var limb: Word
+    staticFor i, 0 .. leftover - 1:
+      limb = limb or (Word(src[bytes - i - 1]) shl ((leftover - i - 1) shl 3))
+
+    result.limbs[words] = limb
+
+template fromBytesCopy(src: openArray[byte]) =
+  if src.len >= result.bits div 8:
+    fromBytesCopyUnrolled(src)
+  else:
+    let
+      bytes = src.len
+      words = bytes shr wordShift
+
+    block bulk: # Unroll this part to reduce in-loop arithmetic
+      staticFor i, 0 ..< result.limbs.len:
+        if i >= words:
+          break bulk
+
+        const pos = i * sizeof(Word)
+        let
+          limb = Word.fromBytes(src.toOpenArray(pos, pos + sizeof(Word) - 1))
+        result.limbs[i] = limb
+
+    let leftover = bytes and wordMask
+    var limb: Word
+    for i in 0 ..< leftover:
+      limb = limb or (Word(src[bytes - i - 1]) shl ((leftover - i - 1) shl 3))
+
+    # No overflow because src.len < result.bits div 8
+    result.limbs[words] = limb
+
+    for i in words + 1 ..< result.limbs.len:
+      result.limbs[i] = 0
+
 func fromBytesBE*[bits: static int](
-    T: typedesc[StUint[bits]],
-    x: openArray[byte]): T {.raises: [], noinit, gcsafe.} =
-  ## Read big endian bytes and convert to an integer. At runtime, v must contain
-  ## at least sizeof(T) bytes. Native endianess is used which is not
-  ## portable! (i.e. use fixed-endian byte array or hex for serialization)
+    T: typedesc[StUint[bits]], src: openArray[byte]
+): T =
+  ## Read big endian bytes and convert to an integer. At runtime, src must contain
+  ## at least sizeof(T) bytes.
+  ## TODO Contrary to docs and the corresponding stew/endians2 function, src is
+  ##      actually padded when short - this may change in the future to match
+  ##      stew where it panics instead
+  when cpuEndian == littleEndian:
+    fromBytesSwap(src)
+  else:
+    fromBytesCopy(src)
 
-  var accum: Word = 0
-  var accumBits: int = 0
-  var dstIdx: int
-
-  when cpuEndian == littleEndian: # src is bigEndian, CPU is little-endian
-    dstIdx = 0
-
-    for srcIdx in countdown(x.len-1, 0):
-      let srcByte = Word(x[srcIdx])
-
-      accum = accum or (srcByte shl accumBits)
-      accumBits += 8
-
-      if accumBits >= WordBitWidth:
-        result.limbs[dstIdx] = accum
-        inc dstIdx
-        accumBits -= WordBitWidth
-        accum = srcByte shr (8 - accumBits)
-
-    if dstIdx < result.limbs.len:
-      result.limbs[dstIdx] = accum
-      for fillIdx in dstIdx+1 ..< result.limbs.len:
-        result.limbs[fillIdx] = 0
-  else:                          # src and CPU are bigEndian
-    dstIdx = result.limbs.len-1
-
-    for srcIdx in countdown(x.len-1, 0):
-      let srcByte = Word(x[srcIdx])
-
-      accum = accum or (srcByte shl accumBits)
-      accumBits += 8
-
-      if accumBits >= WordBitWidth:
-        result.limbs[dstIdx] = accum
-        dec dstIdx
-        accumBits -= WordBitWidth
-        accum = srcByte shr (8 - accumBits)
-
-    if dstIdx > 0:
-      result.limbs[dstIdx] = accum
-      for fillIdx in 0 ..< dstIdx:
-        result.limbs[fillIdx] = 0
+func fromBytesBE*[bits: static int](
+    T: typedesc[StUint[bits]], src: array[bits div 8, byte]
+): T =
+  ## Read big endian bytes and convert to an integer. At runtime, src must contain
+  ## at least sizeof(T) bytes.
+  ## TODO Contrary to docs and the corresponding stew/endians2 function, src is
+  ##      actually padded when short - this may change in the future to match
+  ##      stew where it panics instead
+  when cpuEndian == littleEndian:
+    fromBytesSwapUnrolled(src)
+  else:
+    fromBytesCopyUnrolled(src)
 
 func fromBytesLE*[bits: static int](
-    T: typedesc[StUint[bits]],
-    x: openArray[byte]): T =
-  ## Read little endian bytes and convert to an integer. At runtime, v must
-  ## contain at least sizeof(T) bytes. By default, native endianess is used
-  ## which is not portable! (i.e. use fixed-endian byte array or hex for serialization)
+    T: typedesc[StUint[bits]], src: openArray[byte]
+): T =
+  ## Read little endian bytes and convert to an integer. At runtime, src must
+  ## contain at least sizeof(T) bytes.
+  ## TODO Contrary to docs and the corresponding stew/endians2 function, src is
+  ##      actually padded when short - this may change in the future to match
+  ##      stew where it panics instead
 
-  var accum: Word = 0
-  var accumBits: int = 0
-  var dstIdx: int
+  when cpuEndian == littleEndian:
+    fromBytesCopy(src)
+  else:
+    fromBytesSwap(src)
 
-  when cpuEndian == littleEndian: # src and CPU are little-endian
-    dstIdx = 0
-
-    for srcIdx in 0 ..< x.len:
-      let srcByte = Word(x[srcIdx])
-
-      accum = accum or (srcByte shl accumBits)
-      accumBits += 8
-
-      if accumBits >= WordBitWidth:
-        result.limbs[dstIdx] = accum
-        inc dstIdx
-        accumBits -= WordBitWidth
-        accum = srcByte shr (8 - accumBits)
-
-    if dstIdx < result.limbs.len:
-      result.limbs[dstIdx] = accum
-      for fillIdx in dstIdx+1 ..< result.limbs.len:
-        result.limbs[fillIdx] = 0
-  else:                          # src is little endian, CPU is bigEndian
-    dstIdx = result.limbs.len-1
-
-    for srcIdx in 0 ..< x.len:
-      let srcByte = Word(x[srcIdx])
-
-      accum = accum or (srcByte shl accumBits)
-      accumBits += 8
-
-      if accumBits >= WordBitWidth:
-        result.limbs[dstIdx] = accum
-        dec dstIdx
-        accumBits -= WordBitWidth
-        accum = srcByte shr (8 - accumBits)
-
-    if dstIdx > 0:
-      result.limbs[dstIdx] = accum
-      for fillIdx in 0 ..< dstIdx:
-        result.limbs[fillIdx] = 0
+func fromBytesLE*[bits: static int](
+    T: typedesc[StUint[bits]], src: array[bits div 8, byte]
+): T =
+  when cpuEndian == littleEndian:
+    fromBytesCopyUnrolled(src)
+  else:
+    fromBytesSwapUnrolled(src)
 
 func fromBytes*[bits: static int](
-    T: typedesc[StUint[bits]],
-    x: openArray[byte],
-    srcEndian: Endianness = bigEndian): T {.inline.} =
+    T: typedesc[StUint[bits]], src: openArray[byte], srcEndian: Endianness
+): T =
   ## Read an source bytearray with the specified endianness and
   ## convert it to an integer
-  ## Default to bigEndian
+  ## TODO Unlike the corresponding function in stew/endians that defaults to
+  ##      native endian, this function used to default to big endian - the
+  ##      default has been removed to avoid confusion and runtime surprises
   if srcEndian == littleEndian:
-    result = fromBytesLE(T, x)
+    fromBytesLE(T, src)
   else:
-    result = fromBytesBE(T, x)
+    fromBytesBE(T, src)
 
+func fromBytes*[bits: static int](
+    T: typedesc[StUint[bits]], src: array[bits div 8, byte], srcEndian: Endianness
+): T =
+  ## Read an source bytearray with the specified endianness and
+  ## convert it to an integer
+  ## TODO Unlike the corresponding function in stew/endians that defaults to
+  ##      native endian, this function used to default to big endian - the
+  ##      default has been removed to avoid confusion and runtime surprises
+  if srcEndian == littleEndian:
+    fromBytesLE(T, src)
+  else:
+    fromBytesBE(T, src)
 
 # Signed integer version of above funcs
+# TODO these are not in stew/endians2 :/
 # ------------------------------------------------------------------------------------------
 
-func toBytesLE*[bits: static int](src: StInt[bits]):
-                array[bits div 8, byte] {.inline.} =
+func toBytesLE*[bits: static int](src: StInt[bits]): array[bits div 8, byte] =
   toBytesLE(src.impl)
 
-func toBytesBE*[bits: static int](src: StInt[bits]):
-                array[bits div 8, byte] {.inline.} =
+func toBytesBE*[bits: static int](src: StInt[bits]): array[bits div 8, byte] =
   toBytesBE(src.impl)
 
-func toBytes*[bits: static int](x: StInt[bits], endian: Endianness = bigEndian):
-              array[bits div 8, byte] {.inline.} =
+func toBytes*[bits: static int](
+    x: StInt[bits], endian: Endianness
+): array[bits div 8, byte] =
+  ## TODO Unlike the corresponding function in stew/endians that defaults to
+  ##      native endian, this function used to default to big endian - the
+  ##      default has been removed to avoid confusion and runtime surprises
   toBytes(x.impl, endian)
 
 func fromBytesBE*[bits: static int](
-    T: typedesc[StInt[bits]],
-    x: openArray[byte]): T {.raises: [], noinit, gcsafe, inline.} =
+    T: typedesc[StInt[bits]], x: openArray[byte]
+): T {.raises: [], noinit, gcsafe, inline.} =
   fromBytesBE(type result.impl, x)
 
-func fromBytesLE*[bits: static int](
-    T: typedesc[StInt[bits]],
-    x: openArray[byte]): T {.inline.} =
+func fromBytesLE*[bits: static int](T: typedesc[StInt[bits]], x: openArray[byte]): T =
   fromBytesLE(type result.impl, x)
 
 func fromBytes*[bits: static int](
-    T: typedesc[StInt[bits]],
-    x: openArray[byte],
-    srcEndian: Endianness = bigEndian): T {.inline.} =
+    T: typedesc[StInt[bits]], x: openArray[byte], srcEndian: Endianness
+): T =
+  ## TODO Unlike the corresponding function in stew/endians that defaults to
+  ##      native endian, this function used to default to big endian - the
+  ##      default has been removed to avoid confusion and runtime surprises
   fromBytes(type result.impl, x, srcEndian)
--- tests/test_uint_endians2.nim
+++ tests/test_uint_endians2.nim
@@ -7,7 +7,7 @@
 #
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import ../stint, unittest2, stew/byteutils
+import ../stint, ../stint/endians2, unittest2, stew/byteutils, std/algorithm
 
 template chkToBytesLE(bits: int, hex: string) =
   let x = fromHex(StUint[bits], hex)
@@ -19,19 +19,35 @@ template chkToBytesBE(bits: int, hex: string) =
 
 template chkFromBytesBE(bits: int, hex: string) =
   let x = fromHex(StUint[bits], hex)
-  let z = fromBytesBE(StUint[bits], toByteArrayBE(x))
-  check z == x
+
+  check:
+    x.toBytesBE().toHex() == hex
+
+  let z = fromBytesBE(StUint[bits], toBytesBE(x))
+  check toHex(z) == hex
 
 template chkFromBytesLE(bits: int, hex: string) =
   let x = fromHex(StUint[bits], hex)
-  let z = fromBytesLE(StUint[bits], toByteArrayLE(x))
-  check z == x
+
+  check:
+    x.toBytesLE().toHex() == reversed(hexToSeqByte(hex)).toHex()
+
+  let z = fromBytesLE(StUint[bits], toBytesLE(x))
+  check toHex(z) == hex
 
 template chkEndians(name: untyped) =
   test astToStr(name).substr(3):
     name(64, "abcdef1234567890")
     name(128, "abcdef1234567890abcdef1234567890")
     name(256, "abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890")
+
+    name(72, "a0a1a2a3a4a5a6a7a8")
+    name(80, "a0a1a2a3a4a5a6a7a8a9")
+    name(88, "a0a1a2a3a4a5a6a7a8a9aa")
+    name(96, "a0a1a2a3a4a5a6a7a8a9aaab")
+    name(104, "a0a1a2a3a4a5a6a7a8a9aaabac")
+
+    name(264, "abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890aa")
 
 suite "Testing endians":
   chkEndians(chkToBytesLE)
@@ -40,12 +56,11 @@ suite "Testing endians":
   chkEndians(chkFromBytesBE)
 
   test "Endians give sane results":
-
     check:
-      1.u128.toByteArrayBE() ==
+      1.u128.toBytesBE() ==
         [0'u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 
-      1.u128.toByteArrayLE() ==
+      1.u128.toBytesLE() ==
         [1'u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
       1.u128 == UInt128.fromBytesBE(
@@ -53,3 +68,22 @@ suite "Testing endians":
 
       1.u128 == UInt128.fromBytesLE(
         [1'u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+      1.u128 == UInt128.fromBytesBE(
+        [0'u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 42])
+
+      1.u128 == UInt128.fromBytesLE(
+        [1'u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42])
+
+      1.stuint(120) == StUint[120].fromBytesLE(
+        [1'u8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42])
+
+
+      # TOOD the tests below are not valid according to docs by encode de-facto
+      #      usage in the wild
+
+      1.stuint(128) == StUint[128].fromBytesLE([1'u8, 0, 0, 0, 0, 0, 0, 0])
+      1.stuint(128) == StUint[128].fromBytesBE([0'u8, 0, 0, 0, 0, 0, 0, 0, 1])
+
+      1.stuint(128) == StUint[128].fromBytesLE([1'u8])
+      1.stuint(128) == StUint[128].fromBytesBE([1'u8])
