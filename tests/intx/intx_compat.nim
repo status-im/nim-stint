8ae0b5f3cdc04e68aa2eb9e7aa468a2edb5b0a99
tersec <tersec@users.noreply.github.com>
2026-08-30 05:33:32 +0000

    rm deprecated unsafeAddr (#189)

--- tests/intx/intx_compat.nim
+++ tests/intx/intx_compat.nim
@@ -6,11 +6,11 @@ export intx
 
 template asSt*(val: UInt): auto =
   type TargetType = StUint[val.NumBits]
-  cast[ptr TargetType](unsafeAddr val)[]
+  cast[ptr TargetType](addr val)[]
 
 template asTT*[N: static[int]](arr: array[N, uint64]): auto =
   type TargetType = UInt[N * 64]
-  cast[ptr TargetType](unsafeAddr arr[0])[]
+  cast[ptr TargetType](addr arr[0])[]
 
 template asTT*(x: StUint): auto =
   type TargetType = UInt[x.bits]
